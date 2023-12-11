4ed68b9f9f3497b111de4e61293d0824a464a753
Marko Vlahovic <vlahovic74@gmail.com>
2023-12-11 11:03:10 -0800

    Create github integration page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,10 +21,10 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
+  organizationName: "marko7460", // Usually your GitHub org/user name.
   projectName: "docusaurus", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "log",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
